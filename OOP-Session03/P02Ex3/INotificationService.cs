98f46b52a373da3cb68ac888831228c247340368
Rawan Amer <rwan274ro@gmail.com>
2025-07-26 13:52:38 -0700

    Assignment

---
+++ OOP-Session03/P02Ex3/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Session03.P02Ex3
+{
+    internal interface INotificationService
+    {
+        void SendNotification(string Recipient, string Message);
+    }
+}
